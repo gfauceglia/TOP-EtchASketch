1fa83faee76ca4bc67097854659efa97ce359e12
gfauceglia <98237396+gfauceglia@users.noreply.github.com>
2023-06-07 18:18:17 -0300

    Change in createSquare function, class of squares container

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 function createSquares(n) {
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n * n; i++) {
     let square = document.createElement('div');
     square.className = 'square';
     document.getElementById('squares').appendChild(square);
   }
 }
+
+createSquares(16);
