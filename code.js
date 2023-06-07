183a347e1d4fde4fceba387fd2e7d97207cd10f5
gfauceglia <98237396+gfauceglia@users.noreply.github.com>
2023-06-07 18:25:40 -0300

    Add hover effect on squares

--- code.js
+++ code.js
@@ -2,6 +2,9 @@ function createSquares(n) {
   for (let i = 0; i < n * n; i++) {
     let square = document.createElement('div');
     square.className = 'square';
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
     document.getElementById('squares').appendChild(square);
   }
 }
