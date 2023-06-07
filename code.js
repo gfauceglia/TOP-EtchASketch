acde19fe75b0920e757fd2d3a5c5ce677db0678a
gfauceglia <98237396+gfauceglia@users.noreply.github.com>
2023-06-07 19:01:33 -0300

    Make reset function

--- code.js
+++ code.js
@@ -12,4 +12,15 @@ function createSquares(n) {
   }
 }
 
+function reset() {
+  let n = parseInt(window.prompt('Enter the amount of squares per side you\'d like in your new sketch. Choose a number between 16 and 100.'));
+  console.log(isNaN(n));
+  while (n < 16 || n > 100 || isNaN(n)){
+    n = parseInt(window.prompt('Input not valid. Try again'));
+  }
+  let squaresContainer = document.getElementById('squares');
+  squaresContainer.innerHTML = '';
+  createSquares(n);
+}
+
 createSquares(16);
