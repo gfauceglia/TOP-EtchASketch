90fe4c37661dbe3bc247f901118d99b8bdb69eeb
gfauceglia <98237396+gfauceglia@users.noreply.github.com>
2023-06-07 18:39:07 -0300

    Change createSquare function to set the width in js

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
 function createSquares(n) {
+  let singleSquareWidth = 100 / n;
+
   for (let i = 0; i < n * n; i++) {
     let square = document.createElement('div');
     square.className = 'square';
+    square.style.width = `${singleSquareWidth}%`;
     square.addEventListener('mouseover', () => {
       square.style.backgroundColor = 'black';
     });
